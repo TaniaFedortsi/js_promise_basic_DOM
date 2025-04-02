9a5e0699288599fa8d023379bcdc0ef1edda8ab8
TaniaFedortsi <tania.fdrts@gmail.com>
2025-04-02 13:49:28 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write your code here
+const promise1 = new Promise((resolve) => {
+  document.querySelector('.logo').addEventListener('click', () => {
+    resolve('logo clicked!');
+  });
+});
+
+promise1.then(() => {
+  showMessage('Promise was resolved!', 'message');
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise 2 rejected: Timeout after 3 seconds'));
+  }, 3000);
+});
+
+promise2.catch(() => {
+  showMessage('Promise was rejected!', 'error-message');
+});
+
+function showMessage(text, className) {
+  const message = document.createElement('div');
+
+  message.classList.add('message', className);
+  message.textContent = text;
+  document.querySelector('body').appendChild(message);
+}
